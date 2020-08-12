e321b54f867fc5175e79f0a753f99fad04d95edb
spyrospav <spyrospavlatos4@gmail.com>
2020-08-12 14:31:45 +0300

    Added counter of correct answers

--- test.sh
+++ test.sh
@@ -1,26 +1,35 @@
 #!/bin/bash
 
+echo "Running test cases for semantic analysis..."
+echo -e "----------------------------------------------\n"
 test_dir="./test/"
 count=1
+correct=0
 for testfile in "$test_dir"*.tony
 do
   res=$(awk -v line=3 -v field=3 'NR==line{print $field}' "$testfile")
-  #echo $res
+  name=$(awk -v line=2 -v field=4 'NR==line{print $field}' "$testfile")
   output=$(./tony < $testfile 2>/dev/null)
   if [ "$res" = "Pass" ]; then
     if [ "$output" = "Success." ]; then
-      echo "Test case ${count}: ${testfile} - Success"
+      let correct+=1
+      echo "Test case ${count}: ${name} - Success"
     else
-      echo "Test case ${count}: ${testfile} - Error"
+      echo "Test case ${count}: ${name} - Error"
     fi
   elif [ "$res" = "Fail" ]; then
     if [ "$output" = "Success." ]; then
-      echo "Test case ${count}: ${testfile} - Error"
+      echo "Test case ${count}: ${name} - Error"
     else
-      echo "Test case ${count}: ${testfile} - Success"
+      echo "Test case ${count}: ${name} - Success"
+      let correct+=1
     fi
   else
-    echo "Test case ${count}: ${testfile} - Unknown"
+    echo "Test case ${count}: ${name} - Unknown"
   fi
   let count+=1
 done
+
+let count-=1
+echo -e "----------------------------------------------\n"
+echo "Correct: ${correct}/${count}"
