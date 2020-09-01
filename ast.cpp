e9be00761270b82f32213c3db4afe17f3f75ce55
Pelopidas <jimansensei@gmail.com>
2020-09-01 23:14:54 +0300

    Now ast.cpp is in the repo!!

---
+++ ast.cpp
@@ -0,0 +1,7 @@
+#include "ast.hpp"
+
+llvm::LLVMContext AST::TheContext;
+llvm::IRBuilder<> AST::Builder(TheContext);
+std::unique_ptr<llvm::Module> AST::TheModule;
+std::unique_ptr<llvm::legacy::FunctionPassManager> AST::TheFPM;
+llvm::GlobalVariable *AST::TheVars;
