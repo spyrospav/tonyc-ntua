c99bfbbc4574530ac1d027e4bf62b863636f0fe4
DominusTea <jimansensei@gmail.com>
2021-04-01 22:01:59 +0300

    Updated configure.sh, added testcase.
    Co-Authored-By: Spyros Pavlatos <spyrospav@users.noreply.github.com>

--- configure.sh
+++ configure.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-
 if [ $(id -u) != "0" ]; then
   echo "You must be the superuser to run this script" >&2
   exit 1
 fi
 
 apt-get update
+
+# Install flex
+echo "Installing flex..."
+apt-get -y -qq install flex
+echo "Installed flex"
+
+# Install bison
+echo "Installing bison..."
+apt-get -y -qq install bison
+echo "Installed bison"
 
 # Install gcc
 echo "Installing g++..."
